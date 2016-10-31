6d1245bef80f21100f1bc7a9650865bc21379de8
StephyStuff <stephanie.birch13@gmail.com>
2016-10-31 17:18:50 -0400

    Added function for 'previous' and 'next' buttons so that when you hit either button, the current image unzooms and the next or previous image zooms in.

--- js/code.js
+++ js/code.js
@@ -1,11 +1,40 @@
 (function () {
+  'use strict';
 
   $('main')
-    .find('li')
-    .on('click', function zoomedIn(eventObj) {
-      $(this).addClass('zoomed');
-      $('main').toggleClass('zooming');
-    })
+  .find('li')
+  .on('click', function zoomedIn(eventObj) {
+    $(this).toggleClass('zoomed');
+    $('main').toggleClass('zooming');
+    $(this).append( $('nav'));
+  })
+
+  $('nav')
+  .find('button')
+  .on('click', function zoomPreviousImage(eventObj) {
+    eventObj.stopPropagation();
+
+    var currentImage = $(this).closest('li')
+    var nextImage = $(this).closest('li').next();
+    var previousImage = $(this).closest('li').prev();
+
+    if($(this).is('.next')) {
+      nextImage.append($('nav'));
+      nextImage.toggleClass('zoomed');
+      currentImage.removeClass('zoomed');
+
+    } else {
+
+      previousImage.append($('nav'));
+      previousImage.toggleClass('zoomed');
+      currentImage.removeClass('zoomed');
+    }
+
+  })
+
+
+
+
 
 
 
